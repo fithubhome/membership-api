336624dc5cb9f139ee4abd39b53ab39e0a3ba1c7
Paul Albu <paul.i.albu@gmail.com>
2024-06-07 19:07:50 +0300

    POM config for JPA

--- src/main/java/com/membershipapi/api/model/MembershipType.java
+++ src/main/java/com/membershipapi/api/model/MembershipType.java
@@ -4,15 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @AllArgsConstructor
-public class MembershipType {
+@Entity(name = "membership_type")
 
+public class MembershipType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "price")
     private double price;
 
 }
