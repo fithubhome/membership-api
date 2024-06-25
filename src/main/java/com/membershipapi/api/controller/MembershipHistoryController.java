3ed436fbd1a4d7661900062486c19bd324bf65ee
Paul Albu <paul.i.albu@gmail.com>
2024-06-25 23:45:32 +0300

    cleaned code

--- src/main/java/com/membershipapi/api/controller/MembershipHistoryController.java
+++ src/main/java/com/membershipapi/api/controller/MembershipHistoryController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/addpayment")
-public class PaymentController {
+public class MembershipHistoryController {
     @Autowired
     PaymentService paymentService;
 
--- src/main/java/com/membershipapi/api/model/MembershipHistory.java
+++ src/main/java/com/membershipapi/api/model/MembershipHistory.java
@@ -1,41 +1,41 @@
-package com.membershipapi.api.model;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.UuidGenerator;
-
-import java.sql.Types;
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.UUID;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Setter
-@Getter
-@Entity
-
-public class MembershipHistory {
-    @Id
-    @JdbcTypeCode(Types.VARCHAR)
-    private UUID id;
-
-    private UUID profileId;
-
-    @ManyToOne
-    private MembershipType membershipType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date startDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date endDate;
-
-    private double payment;
-
-
-}
+//package com.membershipapi.api.model;
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import org.hibernate.annotations.JdbcTypeCode;
+//import org.hibernate.annotations.UuidGenerator;
+//
+//import java.sql.Types;
+//import java.time.LocalDate;
+//import java.util.Date;
+//import java.util.UUID;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Setter
+//@Getter
+//@Entity
+//
+//public class MembershipHistory {
+//    @Id
+//    @JdbcTypeCode(Types.VARCHAR)
+//    private UUID id;
+//
+//    private UUID profileId;
+//
+//    @ManyToOne
+//    private MembershipType membershipType;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date startDate;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date endDate;
+//
+//    private double payment;
+//
+//
+//}
